af7952228c6663ca50ef187a7dcde7437faba0cc
DryRunDemon <sajithjmohamedhameem@gmail.com>
2025-11-13 14:27:33 +0530

    Stage 2: always return 200 OK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,18 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exeception {
     ServerSocket serverSocket = new ServerSocket(4221);
     System.out.println("Server started on port 4221...");
     while (true) {
       Socket clientSocket = serverSocket.accept();
       BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      BufferedWriter out = new BufferedWriter(new outputStreamWriter(clientSocket.getOutputStream()));
+      BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
       String line = in.readLine();
       System.out.println("Received request: " + line);
       out.write("HTTP/1.1 200 OK\r\n\r\n");
