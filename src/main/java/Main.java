a560b2d95e701a7ee1f15d6b02d17cb28d5d39e7
DryRunDemon <sajithjmohamedhameem@gmail.com>
2025-11-13 14:16:14 +0530

    Implement always-200-ok HTTP server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Logs from your program will appear here!");
+    ServerSocket serverSocket = new ServerSocket(4221);
+    System.out.println("Server started on port 4221...");
+    while (true) {
+      Socket clientSocket = serverSocket.accept();
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      BufferedWriter out = new BufferedWriter(new outputStreamWriter(clientSocket.getOutputStream()));
+      String line = in.readLine();
+      System.out.println("Received request: " + line);
+      out.write("HTTP/1.1 200 OK\r\n\r\n");
+      out.flush();
 
-    try {
-      ServerSocket serverSocket = new ServerSocket(4221);
-
-      serverSocket.setReuseAddress(true);
-
-      serverSocket.accept();
-      System.out.println("accepted new connection");
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
+      clientSocket.close();
     }
   }
 }
